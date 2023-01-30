c557180798d9a92fa920d3c6ba39a4633f6a0791
suyu06 <suyu.zhang@johnabbottcollege.net>
2023-01-30 11:15:34 -0500

    genereate file tsconfig.json and change config in this file

---
+++ 03-tsConfig/Customer.js
@@ -0,0 +1,35 @@
+"use strict";
+var Customer3 = /** @class */ (function () {
+    //define a constructor
+    function Customer3(theFirst, theLast) {
+        this._firstName = theFirst;
+        this._lastName = theLast;
+    }
+    Object.defineProperty(Customer3.prototype, "firstName", {
+        get: function () {
+            return this._firstName;
+        },
+        set: function (value) {
+            this._firstName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Customer3.prototype, "lastName", {
+        get: function () {
+            return this._lastName;
+        },
+        set: function (value) {
+            this._lastName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Customer3;
+}());
+// create a new instance
+var myCustomer3 = new Customer3("Martin", "Dixon");
+// myCustomer.firstName="Eric";
+// myCustomer.lastName = "Lee";
+console.log(myCustomer3.firstName);
+console.log(myCustomer3.lastName);
